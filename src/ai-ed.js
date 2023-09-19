d578fa9ed692707c9a4185058ccf694bff1a8c2e
Eitan Barylko <eitan@barylko.com>
2023-09-18 17:34:34 -0700

    Changed home button to be the brand

--- src/ai-ed.js
+++ src/ai-ed.js
@@ -3,23 +3,20 @@ import * as Handlebars from "handlebars";
 const headerView = `
 <nav class="navbar header" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-<div class="navbar-item is-size-3 has-text-weight-bold">
-    AI-ED
-</div>
+    <div class="navbar-item is-size-3 has-text-weight-bold">
+        <a href="/index.html">AI-ED</a>
+    </div>
     <a role="button" onClick="activateMenu()" id="nav-burger" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="menu">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
-
-    <div id="menu" class="header-items navbar-menu">
-    <a class="navbar-item button is-rounded is-medium" href="/index.html">Home</a>
+  <div id="menu" class="header-items navbar-menu">
     <a class="navbar-item button is-rounded is-medium" href="/resources.html">Resources</a>
     <a class="navbar-item button is-medium is-rounded" href="https://github.com/ai-ed">Contribute &nearr;</a>
-    </div>
-    </nav>
-
+  </div>
+</nav>
 `;
 
 
